53e5614dc9f5bf165187af393c568ea548952961
Stéphane Klein <contact@stephane-klein.info>
2022-11-24 10:05:20 +0100

    Deploy Docker en myserver1

--- install.sh
+++ install.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 set -ev
 
+# Install Nomad
+wget -qO- https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get install nomad -y
+
+# Install Docker
+sudo apt-get remove docker docker.io
+echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common python3-pip -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) \
+      stable"
+sudo apt-get update -y
+sudo apt-get install -y docker-ce
+# Restart docker to make sure we get the latest version of the daemon if there is an upgrade
+sudo service docker restart
+# Make sure we can actually use docker as the vagrant user
+sudo usermod -aG docker vagrant
+sudo docker --version
